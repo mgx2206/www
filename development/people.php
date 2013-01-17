7d4e3121fa2f624501ec922678937ad0f72be2c6
Denny Gunawan <mgx2206@yahoo.com>
2013-01-17 15:42:13 +1100

    Finish Download, Contact and Documentation section

---
+++ development/people.php
@@ -0,0 +1,10 @@
+<HTML>
+<?php
+    $menu="Contact";
+    $title="People";
+    $dir="contact";
+    $root="..";
+    $include="people.inc";
+    include "$root/include/template.inc"
+?>
+</HTML>
---
+++ download/release-11.07.02.php
@@ -0,0 +1,11 @@
+<HTML>
+<?php
+    $menu="Download";
+    $submenu="Current Release";
+    $title="Current Release";
+    $dir="download";
+    $root="..";
+    $include="release.inc";
+    include "$root/include/template.inc"
+?>
+</HTML>
